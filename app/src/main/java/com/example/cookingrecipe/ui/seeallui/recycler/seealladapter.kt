d10e452a2d10e6f0ee4d4144a99ae1d9ffb932e2
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2023-02-26 21:53:44 +0530

    seeall Fragment implemented

--- app/src/main/java/com/example/cookingrecipe/ui/seeallui/recycler/seealladapter.kt
+++ app/src/main/java/com/example/cookingrecipe/ui/seeallui/recycler/seealladapter.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookingrecipe.R
 import com.example.cookingrecipe.apidata.recipes
+import com.example.cookingrecipe.ui.homeui.homeDirections
 import com.squareup.picasso.Picasso
 
 class seealladapter(val context: Context, val data: recipes, val onsave: () -> Unit): RecyclerView.Adapter<seealladapter.viewhh>(){
@@ -17,11 +19,12 @@ class seealladapter(val context: Context, val data: recipes, val onsave: () -> U
         val image=View.findViewById<ImageView>(R.id.tfoodimg)
         val title=View.findViewById<TextView>(R.id.tfoodname)
         val save=View.findViewById<ImageView>(R.id.save)
+        val view=View
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewhh {
-        val lay=LayoutInflater.from(parent.context).inflate(R.layout.activity_savedrecycler,parent,false)
+        val lay=LayoutInflater.from(parent.context).inflate(R.layout.seeall,parent,false)
         return viewhh(lay)
     }
 
@@ -32,5 +35,9 @@ class seealladapter(val context: Context, val data: recipes, val onsave: () -> U
     override fun onBindViewHolder(holder: viewhh, position: Int) {
         holder.title.text=data.recipes[position].title
         Picasso.get().load(data.recipes[position].image).into(holder.image)
+        holder.image.setOnClickListener{
+            val action= homeDirections.actionHome2ToDetailrecipe2(position,"rec")
+            Navigation.findNavController(holder.view).navigate(action)
+        }
     }
 }
